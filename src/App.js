7e26cd614ed5de5835261b88ac7d1d26d1255ac6
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-07-18 01:53:30 +1000

    fotter added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./Components/Footer";
 import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
 import Offers from "./Components/Offers";
@@ -14,6 +15,7 @@ function App() {
       <Plan />
       <Rooms />
       <Slides />
+      <Footer />
     </div>
   );
 }
